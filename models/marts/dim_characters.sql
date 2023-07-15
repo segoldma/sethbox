80a5c80e59e37b98ce4bac8038f3f0f8b0b1d71a
Seth <sethgoldman1@gmail.com>
2023-07-15 15:02:25 -0400

    Add dim model and removed cruft from project.yml

---
+++ models/marts/dim_characters.sql
@@ -0,0 +1,29 @@
+{{
+    config(materialized='table')
+}}
+
+with characters as (
+    select * from {{ ref('stg_characters') }}
+)
+
+, personality_traits as (
+    select * from {{ ref('stg_personality_traits') }}
+)
+
+, appearance_counts as (
+    select * from {{ ref('stg_appearance_counts') }}
+)
+
+, final as (
+    select
+      characters.character_id
+      , characters.character_name
+      , personality_traits.personality_traits
+      , appearance_counts.episode_count
+
+    from characters
+    left join personality_traits using (character_id)
+    left join appearance_counts using (character_id)
+)
+
+select * from final
